4d9b882d932a482293cc72f5d4833fc3b75131c5
Sam Lanning <sam@samlanning.com>
2019-06-16 12:35:27 -0700

    Fix usage

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const treeSitter = require('remark-tree-sitter');
 
+module.exports = ({ markdownAST }, options) => {
+  console.log('run');
+  const transformer = treeSitter(options)
+
+  return transformer(markdownAST);
+}
+
 module.exports.setParserPlugins = options => {
+  // Hook into setParserPlugins (which is only called once) to validate the options
   try {
     treeSitter.validateOptions(options);
   } catch (e) {
     throw new Error('Invalid options passed to plugin gatsby-remark-tree-sitter: ' + e.toString());
   }
-  return [[treeSitter, options]];
+  return [];
 };
