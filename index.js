61eefcc2d40500c873e797f2da3929c1d1805afb
Sam Lanning <sam@samlanning.com>
2019-06-16 11:25:31 -0700

    Validate options of plugin

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const treeSitter = require('remark-tree-sitter');
 
-module.exports.setParserPlugins = options => [[treeSitter, options]];
+module.exports.setParserPlugins = options => {
+  try {
+    treeSitter.validateOptions(options);
+  } catch (e) {
+    throw new Error('Invalid options passed to plugin gatsby-remark-tree-sitter: ' + e.toString());
+  }
+  return [[treeSitter, options]];
+};
